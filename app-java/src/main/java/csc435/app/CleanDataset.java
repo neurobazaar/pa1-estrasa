0e1c6c75ae711895f1e9b9213ac1081e3aa4d057
estrasa <100318715+estrasa@users.noreply.github.com>
2024-03-18 21:37:46 -0500

    Update CleanDataset.java

--- app-java/src/main/java/csc435/app/CleanDataset.java
+++ app-java/src/main/java/csc435/app/CleanDataset.java
@@ -2,7 +2,6 @@ package csc435.app;
 
 import java.io.BufferedReader;
 import java.io.File;
-//import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -68,7 +67,6 @@ public class CleanDataset {
 					writer.close();
 
 				} catch (IOException e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 				
